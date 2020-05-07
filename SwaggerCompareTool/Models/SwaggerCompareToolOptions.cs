20d297f75db3b85eabd95be26297a6540e137eb6
Stuart Williams <spookdejur@hotmail.com>
2020-05-07 11:42:30 -0700

    Initial Structure

---
+++ SwaggerCompareTool/Models/SwaggerCompareToolOptions.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+using CommandLine.Text;
+
+namespace SwaggerCompareTool.Models
+{
+    public class SwaggerCompareToolOptions
+    {
+        [Option('v', "Verbose", Default = false, HelpText = "Enable Verbose Output")]
+        public bool Verbose { get; set; }
+
+        [Option('c', "Current", Required = true, HelpText = "Current OpenAPI Json")]
+        public string Current { get; set; }
+
+        [Option('p', "Previous", Required = true, HelpText = "Previous OpenAPI Json")]
+        public string Previous { get; set; }
+
+        [Option('w', "web-report", Default = true, HelpText = "HTML Report")]
+        public bool WebReport { get; set; }
+
+        [Option('j', "JsonDump", Default = false, HelpText = "JSON Dump")]
+        public bool JsonDump { get; set; }
+
+        [Option('e', "Excel", Default = false, HelpText = "CSV for Excel")]
+        public bool ExcelCsv { get; set; }
+
+        [Option('r', "RuleFile", Default = "", HelpText = "Rule File")]
+        public string RuleFile { get; set; }
+
+        /// <summary>
+        /// Help
+        /// </summary>
+        [Usage(ApplicationAlias = "SwaggerCompareTool")]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                return new List<Example>() {
+                    new Example("Useful", new SwaggerCompareToolOptions { Current="current.json", Previous="previous.json", ExcelCsv=true, WebReport=true, Verbose=true  }),
+                    new Example("Minimal", new SwaggerCompareToolOptions { Current="current.json", Previous="previous.json", ExcelCsv=true }),
+                    new Example("With Rules", new SwaggerCompareToolOptions { Current="current.json", Previous="previous.json", ExcelCsv=true, RuleFile = "rules.json" })
+                };
+            }
+        }
+
+
+    }
+}
---
+++ SwaggerCompareTool/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using CommandLine;
+using CommandLine.Text;
+
+
+namespace SwaggerCompareTool
+{
+    class Program
+    {
+        #region "Standard Shell Fields"
+        static int exitCode = 0; // Zero is good, not zero is bad
+        #endregion
+
+
+        static void Main(string[] args)
+        {
+            #region "Global error handler"
+            // Notice a not handled exception from UOW will be caught by global error handler
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            #endregion
+            
+            Console.WriteLine($"{HeadingInfo.Default} {CopyrightInfo.Default}");
+
+            Parser.Default.ParseArguments<Models.SwaggerCompareToolOptions>(args)
+                   .WithParsed<Models.SwaggerCompareToolOptions>(o =>
+                   {
+                       var arguments = CommandLine.Parser.Default.FormatCommandLine(o);
+                       Console.WriteLine($"{arguments}");
+
+                       if(!File.Exists(o.Current))
+                       {
+                           Console.Error.WriteLine($"Current JSON Not Found: {o.Current}");
+                           exitCode = -2;
+                       }
+
+                       if (!File.Exists(o.Previous))
+                       {
+                           Console.Error.WriteLine($"Previous JSON Not Found: {o.Previous}");
+                           exitCode = -3;
+                       }
+
+                       if(File.Exists(o.RuleFile))
+                       {
+                           // TODO: Rule file
+                       }
+
+
+
+
+
+                   }).WithNotParsed(errs =>
+                   {
+                       foreach (var e in errs)
+                       {
+                           Console.WriteLine($"Error: {e.Tag}");
+                       }
+                       exitCode = -1;
+                   });
+
+            Environment.ExitCode = exitCode;
+        }
+
+
+        #region "Global Error Handler"
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = new ApplicationException("Unhandled exception caused crash, please see logs");
+            if ((e != null) && (e.ExceptionObject != null))
+            {
+                if (e.ExceptionObject is Exception ex2)
+                {
+                    ex = ex2;
+                }
+            }
+
+            Console.Error.WriteLine($"CurrentDomain_UnhandledException: {ex.Message}");
+            exitCode = -1; // unhandled exception
+        }
+
+        #endregion
+    }
